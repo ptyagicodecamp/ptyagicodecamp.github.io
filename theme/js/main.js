268185cb6d6430108ce6a06b12b8782837410363
ptyagi <ptyagicodecamp@gmail.com>
2018-11-17 22:55:06 -0800

    theme updated

--- theme/js/main.js
+++ theme/js/main.js
@@ -1,1 +1,15 @@
-// Empty for now. Maybe used later.
+'use strict';
+
+$(document).ready(function() {
+    $('.mg-container-social').height($('article').height());
+    $('#mg-panel-social').stick_in_parent({offset_top: 35});
+
+    $('#tipue_search_input').tipuesearch({
+        'show': 10,
+        'mode': 'json',
+        'showURL': false,
+        'descriptiveWords': 75,
+        'highlightEveryTerm': true,
+        'contentLocation': '/tipue_search.json'
+    });
+});
